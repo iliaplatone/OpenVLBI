ed0fd11d1bfbfa9224ac1391e58248673baa7ad8
Ilia Platone <info@iliaplatone.com>
2019-06-24 23:16:04 +0200

    added dependencies

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+sudo add-apt-repository ppa:~mutlaqja -y
 sudo apt-get -qq update
-sudo apt-get install libindi-dev libpthread-stubs0-dev libcfitsio-dev zlib1g-dev cmake
+sudo apt-get install indi-full libpthread-stubs0-dev libcfitsio-dev zlib1g-dev cmake
 
 mkdir -p build
 pushd build
