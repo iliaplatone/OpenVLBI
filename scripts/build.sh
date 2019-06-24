9f3fefbc94c3be70b2a8822e5fcf1b21938827a6
Ilia Platone <info@iliaplatone.com>
2019-06-24 23:47:11 +0200

    finished build script corrections

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,8 +4,8 @@ sudo apt-get -qq update
 sudo apt-get install indi-full libnova-dev libindi-dev libpthread-stubs0-dev libcfitsio-dev zlib1g-dev cmake
 
 mkdir -p build
-pushd build
-cmake ..
+pushd -DCMAKE_INSTALL_PATH=/usr build
+cmake -..
 make
 make install
 popd
