6fcbbd704c46e165a31c8383adb948cdcfa95d45
Ilia Platone <info@iliaplatone.com>
2019-07-09 22:06:31 +0200

    Update vlbi_client.h

--- vlbi_client.h
+++ vlbi_client.h
@@ -17,17 +17,17 @@ public:
     VLBIClient();
     ~VLBIClient();
     
-    virtual void SetCapture(double seconds);
-    virtual void AbortCapture();
-    virtual void SetExposure(double seconds);
-    virtual void AbortExposure();
     virtual void SetFrequency(double centerfrequency);
     virtual void SetSampleRate(double samplingfrequency);
     virtual void SetBadwidth(double bandwidth);
     virtual void SetGain(double gain);
     virtual void SetBPS(int BPS);
+    inline void CreateContext() { context = vlbi_init(); }
+    inline void FreeContext() { vlbi_exit(context); }
     inline void SetContext(vlbi_context ctx) { context = ctx; }
     inline vlbi_context GetContext() { return context; }
+    inline char* GetDFT() { return vlbi_get_plot_dft(response->samplerate, response->frequency, response->bandwidth); }
+    inline char* GetRAW() { return response->plot; }
 
 private:
     vlbi_context context;
