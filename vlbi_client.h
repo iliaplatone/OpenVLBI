1fdeeb4dd8608108301a242d9e6d2db9576799b0
Ilia Platone <info@iliaplatone.com>
2019-07-09 22:36:42 +0200

    Update vlbi_client.h

--- vlbi_client.h
+++ vlbi_client.h
@@ -26,12 +26,16 @@ public:
     virtual void SetBadwidth(double bandwidth);
     virtual void SetGain(double gain);
     virtual void SetBPS(int BPS);
-    virtual void SetTarget(double ra, double dec);
+    inline void SetTarget(double Ra, double Dec) { ra = Ra; dec = Dec; }
     inline void CreateContext() { context = vlbi_init(); }
     inline void FreeContext() { vlbi_exit(context); }
     inline void SetContext(vlbi_context ctx) { context = ctx; }
     inline vlbi_context* GetContext() { return context; }
-    inline char* GetDFT(int u, int v) {
+    inline char* GetDFT(int u, int v, bool earth_tide_observation) {
+        if(earth_tide_observation)
+            plot = vlbi_get_uv_plot_earth_tide(context, correlation_delegate, earth_tide_observation, u, v, new double[] { ra, dec }, frequency, samplerate);
+        else
+            plot = vlbi_get_uv_plot_moving_baseline(context, correlation_delegate, earth_tide_observation, u, v, new double[] { ra, dec }, frequency, samplerate);
         dsp_stream *fft = vlbi_get_fft_estimation(plot);
         char *base64 = (char*)malloc(fft->len * 4 / 3 + 4);
         to64frombits(fft->buf, base64, fft->len);
