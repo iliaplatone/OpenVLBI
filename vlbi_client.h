afe0a1df4846dd2a56c7e3a791afbe4d8d4598b3
Ilia Platone <info@iliaplatone.com>
2019-07-09 22:15:37 +0200

    Update vlbi_client.h

--- vlbi_client.h
+++ vlbi_client.h
@@ -10,6 +10,10 @@
 #include <sys/ioctl.h>
 #include <vlbi.h>
 
+double correlation_delegate(double value1, double value2)
+{
+    return value1 * value2;
+}
 
 class VLBIClient
 {
@@ -22,14 +26,22 @@ public:
     virtual void SetBadwidth(double bandwidth);
     virtual void SetGain(double gain);
     virtual void SetBPS(int BPS);
+    virtual void SetTarget(double ra, double dec);
     inline void CreateContext() { context = vlbi_init(); }
     inline void FreeContext() { vlbi_exit(context); }
     inline void SetContext(vlbi_context ctx) { context = ctx; }
     inline vlbi_context* GetContext() { return context; }
-    inline char* GetDFT() { return vlbi_get_plot_dft(response->samplerate, response->frequency, response->bandwidth); }
-    inline char* GetRAW() { return response->plot; }
+    inline char* GetDFT(int u, int v, bool earth_tide_observation) { return vlbi_get_plot_dft(context, correlation_delegate, earth_tide_observation, u, v, new double[] { ra, dec }, frequency, samplerate); }
+    inline char* GetRAW(int u, int v) { return return vlbi_get_plot_raw(context, correlation_delegate, earth_tide_observation, u, v, new double[] { ra, dec }, frequency, samplerate); }
 
 private:
+    double frequency;
+    double samplingfrequency;
+    double bandwidth;
+    double gain;
+    double BPS;
+    double ra;
+    double dec;
     vlbi_context context;
 };
 #endif // _VLBI_CLIENT_H
