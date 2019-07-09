158e3a082cc137e194dfe26dd683dc9bea081bb7
Ilia Platone <info@iliaplatone.com>
2019-07-09 19:53:08 +0200

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo add-apt-repository ppa:~mutlaqja -y
 sudo apt-get -qq update
-sudo apt-get install indi-full libnova-dev libindi-dev libpthread-stubs0-dev libcfitsio-dev zlib1g-dev cmake
+sudo apt-get install build-essential indi-full json-glib libnova-dev libindi-dev libpthread-stubs0-dev libcfitsio-dev zlib1g-dev cmake
 
 mkdir -p build
 pushd build
