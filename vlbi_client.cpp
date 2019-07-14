22ee4124f78dfd3726fff0a65f1beaf72df4768c
Ilia Platone <info@iliaplatone.com>
2019-07-14 16:41:03 +0800

    updated vlbi_client.cpp

--- vlbi_client.cpp
+++ vlbi_client.cpp
@@ -134,7 +134,7 @@ int main(int argc, char** argv)
             }
         }
         else {
-            fprintf(stderr, "commands: \nadd\n\tcontext name\n\tset\n\tconnection [on|off]\n\tGetContext() name\n\tparking [on|off]\n\ttracking [on|off]\n\ttarget ra,dec\n\tfrequency freq\n\tsamplerate freq\n\tbandwidth freq\n\tbitspersample bps\n\tgain value\n\tcapture time\n\tmodel name\nget\n\tobservation [fft|mdl|raw]");
+            fprintf(stderr, "commands: \nadd\n\tcontext name\nset\n\tconnection [on|off]\n\tcontext name\n\tparking [on|off]\n\ttracking [on|off]\n\ttarget ra,dec\n\tfrequency freq\n\tsamplerate freq\n\tbandwidth freq\n\tbitspersample bps\n\tgain value\n\tcapture time\n\tmodel name\nget\n\tobservation [fft|mdl|raw]");
         }
 	client->Run(cmd, arg, value);
     }
