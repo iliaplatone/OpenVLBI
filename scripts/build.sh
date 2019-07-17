e89ba9a0c7a07cc8c301bcfe441d0a117c90bb6c
Ilia Platone <info@iliaplatone.com>
2019-07-17 18:37:35 +0800

    reverted changes in vlbi_server and tests

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo add-apt-repository ppa:~mutlaqja -y
 sudo apt-get -qq update
-sudo apt-get install build-essential indi-full libnova-dev libindi-dev libpthread-stubs0-dev libcfitsio-dev zlib1g-dev libjson-c-dev cmake
+sudo apt-get install indi-full libnova-dev libindi-dev libpthread-stubs0-dev libcfitsio-dev zlib1g-dev cmake
 
 mkdir -p build
 pushd build
--- scripts/test.sh
+++ scripts/test.sh
@@ -4,13 +4,9 @@
 set -x -e
 
 port=8000
-
-sudo killall vlbi_client_indi -9 2>/dev/null || true
-sleep 2
 sudo killall indiserver -9 2>/dev/null || true
-rm -f /tmp/INDIfifo
-mkfifo /tmp/INDIfifo
-indiserver -f /tmp/INDIfifo -p 7624 2>/dev/null 1>/dev/null &
+sudo killall vlbi_client_indi -9 2>/dev/null || true
+vlbi_server start
 sleep 10
 p=0
 while (( $p<$1 )); do
@@ -18,17 +14,15 @@ while (( $p<$1 )); do
 	current_port=$(( $port+$p ))
 	indiserver -p $current_port indi_simulator_ccd indi_simulator_telescope indi_simulator_dome indi_simulator_gps 2>/dev/null 1>/dev/null &
 	sleep 2
-	echo "start localhost:$current_port" > /tmp/INDIfifo
+	vlbi_server add node "localhost:$current_port"
 	p=$(( $p+1 ))
 done
 sleep 2
-vlbi_server start INDI
+vlbi_server set connection on
 sleep 2
 vlbi_server add context test
 sleep 2
 vlbi_server set context test
-sleep 2
-vlbi_server set connection on
 sleep 2
 vlbi_server set parking off
 sleep 2
