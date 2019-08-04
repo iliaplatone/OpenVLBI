0c32c0fbe3fe3b9d31fe8e5d786d681fbd874b9d
Ilia Platone <info@iliaplatone.com>
2019-08-04 18:24:37 +0200

    updated tests

--- scripts/test_client.sh
+++ scripts/test_client.sh
@@ -5,14 +5,15 @@ num_nodes=$2
 freq=$4
 sr=$5
 radec=$6
-duration=$(( $sr*$3/256 ))
+bps=32
+duration=$(( $sr*$3*$bps/2048 ))
 time=$7
 
 echo add context test
 echo set context test
 echo set frequency $freq
 echo set bandwidth $bw
-echo set bitspersample 8
+echo set bitspersample $bps
 echo set samplerate $sr
 
 p=0
@@ -21,11 +22,12 @@ while [ $duration -gt 0 ]; do
 	scripts/sine.sh 127 triwave
 	duration=$(( $duration-1 ))
 done | base64 > $tmpimg
+
 while (( $p<$num_nodes )); do
-	echo add node node$p,13.0027$RANDOM,44.0027$RANDOM,100.$RANDOM,$tmpimg,$time
+	echo add node node$p,13.27$RANDOM,44.27$RANDOM,100.$RANDOM,$tmpimg,$time
 	p=$(( $p+1 ))
 done
-rm -f $tmpimg
+#rm -f $tmpimg
 
 echo set target $radec
 echo set resolution $size
