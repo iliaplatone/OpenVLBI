0c8a5191f98502e9bb04798b4859992a848a4ab2
Ilia Platone <info@iliaplatone.com>
2021-06-14 21:00:08 +0200

    Testing in circleci

--- debian/files.openvlbi-bin
+++
@@ -1,2 +0,0 @@
-vlbi_client_dummy,/usr/bin/
-vlbi_client_indi,/usr/bin/
--- files.libopenvlbi.cmake
+++ files.libopenvlbi.cmake
@@ -1,3 +1,3 @@
 libopenvlbi.so,/usr/lib/
-libopenvlbi.so.@VLBI_VERSION_MAJOR@/usr/lib/
+libopenvlbi.so.@VLBI_VERSION_MAJOR@,/usr/lib/
 libopenvlbi.so.@VLBI_VERSION_STRING@,/usr/lib/
