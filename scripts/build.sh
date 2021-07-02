538534954f7fafc9152a9250fbfa0025764db3f1
Ilia Platone <info@iliaplatone.com>
2021-07-02 02:33:52 +0200

    Updated debian folder

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,9 +2,9 @@
 export version=$(head -n 1 debian/changelog | tr -d [a-z:\(:\):=:\;:\ ] | tr -d '\n')
 export arch=$(dpkg --print-architecture)
 
-sudo add-apt-repository ppa:~mutlaqja/ppa
+sudo add-apt-repository -y ppa:~mutlaqja/ppa
 sudo apt-get update
-sudo apt-get install indi-full libindi-dev libnova-dev libfftw3-dev libcfitsio-dev cdbs cmake build-essential fakeroot devscripts
-cd ../; tar zcvf libopenvlbi_${version}.orig.tar.gz OpenVLBI; cd OpenVLBI; dpkg-buildpackage -us;
+sudo apt-get install -y indi-full libindi-dev libnova-dev libfftw3-dev libcfitsio-dev cdbs cmake dpkg-dev build-essential fakeroot devscripts
+dpkg-buildpackage -us -sa -rfakeroot;
 sudo dpkg -i ../libopenvlbi_${version}_${arch}.deb
 sudo dpkg -i ../openvlbi-bin_${version}_${arch}.deb
