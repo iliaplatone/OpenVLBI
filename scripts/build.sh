21b906eeb55d5578c4649cb322d4d21cb7f619f9
Ilia Platone <info@iliaplatone.com>
2021-07-02 15:00:45 +0200

    Updated build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,6 @@ export arch=$(dpkg --print-architecture)
 sudo add-apt-repository -y ppa:~mutlaqja/ppa
 sudo apt-get update
 sudo apt-get install -y indi-full libindi-dev libnova-dev libfftw3-dev libcfitsio-dev cdbs cmake dpkg-dev build-essential fakeroot devscripts
-dpkg-buildpackage -us -sa -rfakeroot;
+dpkg-buildpackage  -b -rfakeroot -us -uc;
 sudo dpkg -i ../libopenvlbi_${version}_${arch}.deb
 sudo dpkg -i ../openvlbi-bin_${version}_${arch}.deb
