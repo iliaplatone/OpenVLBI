7792838fee1f58a805aa3264e18e6f535ae8c67d
Ilia Platone <info@iliaplatone.com>
2021-12-11 01:09:30 +0100

    updated test script

--- scripts/json_to_image.sh
+++ scripts/json_to_image.sh
@@ -5,7 +5,7 @@
 json=$1
 image=$2
 
-response=$(vlbi_client_json -t 1 -f $json)
+response=$(vlbi_server_json -t 1 -f $json)
 buffer=$(echo $response | jq .model.buffer | tr -d '"')
 format=$(echo $response | jq .model.format | tr -d '"')
 echo $buffer | base64 -d > $image.$format
