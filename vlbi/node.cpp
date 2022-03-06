31a6a9dac6b2eccf9634df7beae11ef36e8262a4
Ilia Platone <info@iliaplatone.com>
2022-03-06 15:26:31 +0100

    Remove unused calls in node creation

--- vlbi/node.cpp
+++ vlbi/node.cpp
@@ -30,9 +30,6 @@ VLBINode::VLBINode(dsp_stream_p stream, const char* name, int index, bool geogra
         Stream->phase = dsp_stream_copy(Stream);
         dsp_buffer_set(Stream->phase->buf, Stream->phase->len, 0.0);
     }
-    getStream()->align_info.dims = getStream()->dims;
-    dsp_stream_add_dim(getStream(), 1);
-    dsp_stream_alloc_buffer(getStream(), getStream()->len);
     Name = (char*)calloc(150, 1);
     sprintf(Name, "%s", name);
     Index = index;
