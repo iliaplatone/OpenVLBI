7d50cbe5863af42f2630745856fab6dda0ad3e42
Ilia Platone <info@iliaplatone.com>
2022-03-19 21:37:54 +0100

    Add indilib repository

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,10 +4,13 @@ dir=$(dirname $0)
 os=$($dir/determine-os.sh)
 
 if [ "$os" == "Debian" ] || [ "$os" == "Ubuntu" ]; then
+ if [ "$os" == "Ubuntu" ]; then
+  add-apt-repository -y ppa:~mutlaqja/ppa
+ fi
  version=$(head -n 1 debian/changelog | tr -d [a-z:\(:\):=:\;:\ ] | tr -d '\n')
  arch=$(dpkg --print-architecture)
  apt-get update
- apt-get install -y doxygen libfftw3-dev libjpeg-dev libpng-dev libcfitsio-dev cdbs cmake dpkg-dev build-essential fakeroot devscripts jq
+ apt-get install -y doxygen libindi-dev libfftw3-dev libjpeg-dev libpng-dev libcfitsio-dev cdbs cmake dpkg-dev build-essential fakeroot devscripts jq
  dpkg-buildpackage  -b -rfakeroot -us -uc;
  mkdir -p packages
  mv ../libopenvlbi1_${version}_*.deb packages/
