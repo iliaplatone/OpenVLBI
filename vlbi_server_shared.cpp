25811feb836a3ceab4bb0cf7ff1d66041333f6fc
Ilia Platone <info@iliaplatone.com>
2024-11-13 21:35:54 +0100

    Do not use dummy server in shared server library

--- vlbi_server_shared.cpp
+++ vlbi_server_shared.cpp
@@ -1,5 +1,61 @@
-#include "vlbi_server_dummy.h"
+/*  OpenVLBI - Open Source Very Long Baseline Interferometry
+*   Copyright © 2017-2023  Ilia Platone
+*
+*   This program is free software; you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation; either version 2 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License along
+*   with this program; if not, write to the Free Software Foundation, Inc.,
+*   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <signal.h>
+#include <vlbi.h>
+#include <fitsio2.h>
 #include "vlbi_server_shared.h"
+
+using namespace VLBI;
+
+class SHAREDServer : public VLBI::Server
+{
+public:
+    SHAREDServer();
+    ~SHAREDServer();
+
+    int Init(int argc, char** argv);
+    void Parse();
+};
+
+SHAREDServer::SHAREDServer()
+    : VLBI::Server::Server()
+{
+}
+
+SHAREDServer::~SHAREDServer()
+{
+}
+
+int SHAREDServer::Init(int argc, char** argv)
+{
+    return VLBI::Server::Init(argc, argv);
+}
+
+void SHAREDServer::Parse()
+{
+    VLBI::Server::Parse();
+}
+
+Server *VLBI::server = (Server*)new SHAREDServer();
 
 void VLBI_Server_Parse() { VLBI::server->Parse(); }
 void VLBI_Server_addContext(const char *name) { VLBI::server->addContext(name); }
