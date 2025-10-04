3a8bde27f0c7fa9a85e7cbf914c15b19cdab71c3
Ilia Platone <info@iliaplatone.com>
2025-10-04 04:18:14 +0200

    Remove dependency from OpenCV

--- vlbi/feature.cpp
+++ vlbi/feature.cpp
@@ -25,88 +25,18 @@
 #include <modelcollection.h>
 #include <base64.h>
 #include <thread>
-#include <opencv2/opencv.hpp>
-#include <opencv2/features2d.hpp> // Include for feature detection
 
 using namespace std;
-using namespace cv;
 
 extern NodeCollection *vlbi_nodes;
-
-static Mat feature_extract(dsp_stream_p stream)
-{
-    Mat cv_stream(stream->dims, stream->sizes, CV_64F, static_cast<void*>(stream->buf));
-    if (cv_stream.empty()) {
-        perr("Error reading stream data!");
-        return cv_stream;
-    }
-
-    Ptr<Feature2D> extractor;
-    //extractor = AKAZE::create();
-    //extractor = BRISK::create();
-    extractor = ORB::create();
-
-    vector<KeyPoint> keypoints;
-    Mat descriptors;
-
-    extractor->detectAndCompute(cv_stream, Mat(), keypoints, descriptors);
-    return descriptors;
-}
-
-static vector<DMatch> feature_filter(Mat stream, Mat matrix)
-{
-    int i;
-    Ptr<DescriptorMatcher> matcher;
-    //matcher = BFMatcher::create();
-    matcher = FlannBasedMatcher::create();
-    vector<DMatch> matches;
-    matcher->match(stream, matrix, matches);
-
-    double min_dist = DBL_MAX;
-    for( int i = 0; i < stream.rows; i++ ) {
-        double dist = matches[i].distance;
-        if( dist < min_dist ) min_dist = dist;
-    }
-    double good_match_threshold = pow(2.0, 0.5) * min_dist;
-    vector<DMatch> good_matches;
-    for(i = 0; i < stream.rows; i++ ) {
-        if( matches[i].distance < good_match_threshold ) {
-            good_matches.push_back( matches[i]);
-        }
-    }
-    return good_matches;
-}
 
 double vlbi_compare_models(vlbi_context ctx, const char *model1, const char *model2)
 {
     pfunc;
-    NodeCollection *nodes = (ctx != nullptr) ? (NodeCollection*)ctx : vlbi_nodes;
-    if(!vlbi_has_model(ctx, model1))
-        return 0;
-    if(!vlbi_has_model(ctx, model2))
-        return 0;
-    dsp_stream_p stream1 = nodes->getModels()->get(model1);
-    dsp_stream_p stream2 = nodes->getModels()->get(model2);
-    unsigned int i;
-    Mat cv_stream1 = feature_extract(stream1);
-    if (cv_stream1.empty()) {
-        perr("Error reading stream data!");
-        return 0;
-    }
-    Mat cv_stream2 = feature_extract(stream2);
-    if (cv_stream2.empty()) {
-        perr("Error reading matrix data!");
-        return 0;
-    }
-    vector<DMatch> matches = feature_filter(cv_stream1, cv_stream2);
-    if (matches.size() < 1) {
-        perr("Error reading matrix data!");
-        return 0;
-    }
-    double score = 0;
-    for(i = 0; i < matches.size(); i++ ) {
-        score += matches.size() / matches[i].distance;
-    }
-    score /= matches.size();
+    double score;
+    int num_features = 100;
+    dsp_stream_p stream1 = vlbi_get_model(ctx, model1);
+    dsp_stream_p stream2 = vlbi_get_model(ctx, model2);
+    score = dsp_align_get_offset(stream1, stream2, 3, 0.0, num_features);
     return score;
 }
