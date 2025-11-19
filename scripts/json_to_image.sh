a968c52fd01da0eca860289d4568a0bee7b77e68
Ilia Platone <info@iliaplatone.com>
2025-11-19 19:54:51 +0000

    Updated json_to_image.sh

--- scripts/json_to_image.sh
+++ scripts/json_to_image.sh
@@ -4,9 +4,8 @@
 
 json=$1
 
-response=$(vlbi_server_json -t 4 -v -f "$json")
-buffer=$(echo $response | jq .model.buffer | tr -d '"')
-name=$(echo $response | jq .model.name | tr -d '"')
-format=$(echo $response | jq .model.format | tr -d '"')
-echo $buffer
-#| base64 -d > "$name.$format"
+response=$(vlbi_server_json -t 4 -v -f $json -o $json.response)
+buffer=$(cat $json.response | jq .model.buffer | tr -d '"')
+name=$(cat $json.response | jq .model.name | tr -d '"')
+format=$(cat $json.response | jq .model.format | tr -d '"')
+echo $buffer | base64 -d > "$name.$format"
